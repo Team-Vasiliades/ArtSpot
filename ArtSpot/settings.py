4a8524fe4cf63a23709cf66c09d90aede5d255c5
yashnamdeo18x <namdeoyash192@gmail.com>
2025-02-19 05:13:56 +0530

    Fixed like/dislike issue, updated URLs, and adjusted templates

--- ArtSpot/settings.py
+++ ArtSpot/settings.py
@@ -79,9 +79,9 @@ WSGI_APPLICATION = 'ArtSpot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'skillsyncdb',
-        'USER': 'Sayman',
-        'PASSWORD': 'Sayman@2007',
+        'NAME': 'ArtSpot',
+        'USER': 'root',
+        'PASSWORD': '@Yash514',
         'HOST': 'localhost',
         'PORT': '3306', 
     }
@@ -133,3 +133,12 @@ STATICFILES_DIRS= [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'  # Optional, redirects to login after logout
+LOGIN_URL = '/users/login/'
+
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- home/urls.py
+++ home/urls.py
@@ -13,12 +13,10 @@ urlpatterns = [
     path('', views.landing, name='landing'),  # Redirect after login
     path('feed/', views.feed, name='feed'),
     path("add_comment/<int:post_id>/", views.add_comment, name="add_comment"),
-    path('dislike/<int:post_id>/', views.like_dislike_post, {'action': 'dislike'}, name='dislike_post'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('profile/<str:username>/', views.profile_view, name='profile'),
     path('follow/<str:username>/', views.follow_unfollow, name='follow_unfollow'),
     path('post/<int:post_id>/', views.post_detail_view, name='post_detail_view'),
-    path('like/<int:post_id>/<str:action>/', views.like_dislike_post, name='like_post'),
-    path('dislike/<int:post_id>/', views.like_dislike_post, {'action': 'dislike'}, name='dislike_post'),
+    path('like_dislike/<int:post_id>/<str:action>/', views.like_dislike_post, name='like_dislike_post'),
     path("create_post/", views.create_post, name="create_post"),
     ]
